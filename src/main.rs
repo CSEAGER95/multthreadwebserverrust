9144ff5bf3e6fe57f89eb569b5746f429cec3584
CSEAGER95 <seagerjc@gmail.com>
2026-01-28 19:50:53 -0500

    fixed the file path errors and created the 404 file

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,18 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    
+    let request_line = &http_request[0];
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "src/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND", "src/404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
