cb31ee03bb497815f20213560f24383aa50d90a0
CSEAGER95 <seagerjc@gmail.com>
2026-01-28 19:46:40 -0500

    single thread responds to one request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+use std::{
+    fs,
+    io::{BufReader,prelude::*},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let addr = "127.0.0.1:7878"; //this is an ip addr that represents my computer, the port is a random port that usually doesn't accept HTTP requests.
@@ -7,6 +11,25 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
